bdb7cec109a19bf53751b4cea4fdfd27174179e6
rajsachin7061 <rajsachin7061@gmail.com>
2025-06-08 23:42:35 +0530

    Add files via upload

---
+++ TwoAddNumber.java
@@ -0,0 +1,11 @@
+public class TwoAddNumber {
+
+    public static void main(String[] args) {
+        int a = 44;
+        int b = 55;
+
+        int Add = a + b;
+
+        System.out.println(Add);
+    }
+}
