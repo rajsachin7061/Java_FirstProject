46da3e1e61eaa067b6acea24dac11bfc6138ea7b
rajsachin7061 <rajsachin7061@gmail.com>
2025-06-08 23:43:24 +0530

    Add files via upload

---
+++ SubTwNumber.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class SubTwNumber {
+    public static void main(String[] args) {
+        //Create object for take a input
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+     //Subtract the input value
+        int Sub = a + b;
+        System.out.println(Sub);
+        sc.close();
+    }
+}
